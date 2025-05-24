8436f1655df664d74a7a71e1f263759841e1408f
Hana Fejzic <hana.fejzic@edu.fit.ba>
2025-05-24 12:24:58 +0200

    Narudzbe list table pagination

--- UI/eventsappadmin/lib/screens/narudzbe_list_screen.dart
+++ UI/eventsappadmin/lib/screens/narudzbe_list_screen.dart
@@ -174,82 +174,6 @@ class _NarudzbeListScreenState extends State<NarudzbeListScreen>
         ));
   }
 
-  Widget _buildDataListView() {
-    return LayoutBuilder(builder: (context, constraints) {
-      return SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minWidth: constraints.maxWidth,
-            ),
-            child: DataTable(
-                columns: [
-                  DataColumn(
-                    label: Expanded(
-                      child: Text(
-                        'Broj narudžbe',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Expanded(
-                      child: Text(
-                        'Datum',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Expanded(
-                      child: Text(
-                        'Korisnik',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Expanded(
-                      child: Text(
-                        'Cijena',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Expanded(
-                      child: Text(
-                        'Detalji',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ],
-                rows: result?.result
-                        .map((Narudzba e) => DataRow(cells: [
-                              DataCell(Text(
-                                e.brojNarudzbe.toString(),
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              )),
-                              DataCell(Text(
-                                  "${e.datum?.day}.${e.datum?.month}.${e.datum?.year}.")),
-                              DataCell(Text(e.korisnickoIme ?? "")),
-                              DataCell(Text(formatCijena(e.cijena))),
-                              DataCell(IconButton(
-                                  icon: const Icon(Icons.remove_red_eye),
-                                  color: Color.fromRGBO(44, 152, 240, 1),
-                                  splashRadius: 20,
-                                  hoverColor: Color.fromRGBO(224, 224, 224, 1),
-                                  onPressed: () {
-                                    _buildNarudzbaDetails(e!.narudzbaId!);
-                                  })),
-                            ]))
-                        .toList() ??
-                    []),
-          ));
-    });
-  }
-
   _buildNarudzbaDetails(int narudzbaId) {
     showDialog<String>(
       context: context,
@@ -266,204 +190,108 @@ class _NarudzbeListScreenState extends State<NarudzbeListScreen>
     );
   }
 
-  /* _buildNarudzbaDetails() {
-    showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-              title: Text('Detalji o narudžbi', style: h2),
-              content: isLoading
-                  ? const CircularProgressIndicator()
-                  : //Container()
-                  Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                  height: 80,
-                                  width: 80,
-                                  margin: const EdgeInsets.only(right: 20),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(50)),
-                                  child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(50),
-                                      child: k != null
-                                          ? _buildProfilna(k.slika)
-                                          : Image.asset(
-                                              "assets/images/blankprofile.jpg",
-                                              fit: BoxFit.cover))),
-                              Container(
-                                  width: 250,
-                                  alignment: Alignment.topLeft,
-                                  //color: Colors.red,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "${k?.ime} ${k?.prezime}",
-                                        textAlign: TextAlign.left,
-                                        style: TextStyle(
-                                            color: const Color.fromARGB(
-                                                255, 71, 70, 70),
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                      Text("@${k?.korisnickoIme}",
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                              color: const Color.fromARGB(
-                                                  255, 90, 89, 89),
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w300)),
-                                      Text("${k?.email}",
-                                          textAlign: TextAlign.left,
-                                          style: TextStyle(
-                                              color: Colors.grey,
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.w300))
-                                    ],
-                                  ))
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text("Adresa:",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: const Color.fromARGB(
-                                                    255, 71, 70, 70),
-                                                fontSize: 16)),
-                                        const SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text("${k?.adresa}, ${k?.drzava}",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.grey,
-                                                fontSize: 16))
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text("Telefon:",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: const Color.fromARGB(
-                                                    255, 71, 70, 70),
-                                                fontSize: 16)),
-                                        const SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text("${k?.telefon}",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.grey,
-                                                fontSize: 16))
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text("Status:",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: const Color.fromARGB(
-                                                    255, 71, 70, 70),
-                                                fontSize: 16)),
-                                        const SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text(
-                                            k?.status == true
-                                                ? "Aktivan"
-                                                : "Deaktiviran",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.grey,
-                                                fontSize: 16))
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Text("Račun kreiran:",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: const Color.fromARGB(
-                                                    255, 71, 70, 70),
-                                                fontSize: 16)),
-                                        const SizedBox(
-                                          width: 5,
-                                        ),
-                                        Text(
-                                            "${k?.created.day}.${k?.created.month}.${k?.created.year}.",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.grey,
-                                                fontSize: 16))
-                                      ],
-                                    )
-                                  ])
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          Row(children: [
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("Narudžbe",
-                                      textAlign: TextAlign.start, style: h2),
-                                  const Divider(
-                                    color: Color.fromARGB(255, 145, 145, 145),
-                                    height: 5,
-                                    thickness: 0.7,
-                                  ),
-                                  k?.narudzbes == null || k!.narudzbes!.isEmpty
-                                      ? const Text(
-                                          "Korisnik nije izvršio nijednu narudžbu.",
-                                          textAlign: TextAlign.left,
-                                        )
-                                      : const Text("ima nesto")
-                                ]),
-                          ])
-                        ],
-                      )
-                    ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.pop(context, 'Zatvori'),
-                  child: const Text('Zatvori'),
+  Widget _buildDataListView() {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: constraints.maxWidth,
+              maxWidth: constraints.maxWidth,
+            ),
+            child: PaginatedDataTable(
+              header: const Text('Narudžbe'),
+              columns: [
+                DataColumn(
+                  label: Text(
+                    'Broj narudžbe',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Datum',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Korisnik',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Cijena',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DataColumn(
+                  label: Text(
+                    'Detalji',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ],
-            ));
-  }*/
+              source:
+                  _MyDataSource(result?.result ?? [], _buildNarudzbaDetails),
+              rowsPerPage: 5,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
 
-  Image _buildProfilna(String? img) {
-    if (img == null || img == "") {
+Image _buildProfilna(String? img) {
+  if (img == null || img == "") {
+    return Image.asset('assets/images/blankprofile.jpg', fit: BoxFit.cover);
+  } else {
+    try {
+      Image slika = Image.memory(base64Decode(img), fit: BoxFit.cover);
+      return slika;
+    } on Exception catch (e) {
       return Image.asset('assets/images/blankprofile.jpg', fit: BoxFit.cover);
-    } else {
-      try {
-        Image slika = Image.memory(base64Decode(img), fit: BoxFit.cover);
-        return slika;
-      } on Exception catch (e) {
-        return Image.asset('assets/images/blankprofile.jpg', fit: BoxFit.cover);
-      }
     }
   }
+}
+
+class _MyDataSource extends DataTableSource {
+  final List<Narudzba> narudzbe;
+  final void Function(int narudzbaId) buildNarudzbaDetails;
+
+  _MyDataSource(this.narudzbe, this.buildNarudzbaDetails);
+
+  @override
+  DataRow? getRow(int index) {
+    if (index >= narudzbe.length) return null;
+    final e = narudzbe[index];
+    return DataRow(cells: [
+      DataCell(Text(e.brojNarudzbe.toString(),
+          style: const TextStyle(fontWeight: FontWeight.bold))),
+      DataCell(Text("${e.datum?.day}.${e.datum?.month}.${e.datum?.year}.")),
+      DataCell(Text(e.korisnickoIme ?? "")),
+      DataCell(Text(formatCijena(e.cijena))),
+      DataCell(IconButton(
+        icon: const Icon(Icons.remove_red_eye),
+        color: const Color.fromRGBO(44, 152, 240, 1),
+        splashRadius: 20,
+        hoverColor: const Color.fromRGBO(224, 224, 224, 1),
+        onPressed: () {
+          buildNarudzbaDetails(e.narudzbaId!);
+        },
+      )),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => narudzbe.length;
+
+  @override
+  int get selectedRowCount => 0;
 }
