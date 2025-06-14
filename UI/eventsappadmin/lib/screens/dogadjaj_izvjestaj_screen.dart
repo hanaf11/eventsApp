3638a7826cf89a0f6301828532009174368c30c0
Hana Fejzic <hana.fejzic@edu.fit.ba>
2025-06-14 15:04:25 +0200

    First report downloaded

--- UI/eventsappadmin/lib/screens/dogadjaj_izvjestaj_screen.dart
+++ UI/eventsappadmin/lib/screens/dogadjaj_izvjestaj_screen.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:eventsappadmin/models/dogadjaji_report_response.dart';
 import 'package:eventsappadmin/providers/dogadjaj_provider.dart';
 import 'package:eventsappadmin/widgets/master_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:graphic/graphic.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:provider/provider.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/rendering.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class DogadjajIzvjestajScreen extends StatefulWidget {
   int? selected = 6;
@@ -26,6 +35,12 @@ class _DogadjajIzvjestajScreenState extends State<DogadjajIzvjestajScreen> {
     {'genre': 'Shooter', 'sold': 350},
     {'genre': 'Other', 'sold': 150},
   ];
+  //final GlobalKey _dogadjajiCaptureKey = GlobalKey();
+  final GlobalKey _eventsByStatusKey = GlobalKey();
+  final GlobalKey _eventsByCategoryKey = GlobalKey();
+  final GlobalKey _top3EventsKey = GlobalKey();
+  final GlobalKey _mostViewedEventsKey = GlobalKey();
+  final GlobalKey _mostSavedEventsKey = GlobalKey();
 
   _DogadjajIzvjestajScreenState();
 
@@ -51,6 +66,86 @@ class _DogadjajIzvjestajScreenState extends State<DogadjajIzvjestajScreen> {
     print(result?.eventsByStatus);
   }
 
+  Future<Uint8List> _captureWidgetAsImage(GlobalKey key) async {
+    // find the RenderRepaintBoundary
+    RenderRepaintBoundary boundary =
+        key.currentContext!.findRenderObject() as RenderRepaintBoundary;
+
+    ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
+
+  Future<void> exportPdf() async {
+    print("export kliknut");
+    try {
+      final pdf = pw.Document();
+
+      var keysList = [];
+      var chartImagesList = [];
+      if (result?.eventsByStatus != null) keysList.add(_eventsByStatusKey);
+      if (result?.eventsByCategory != null) keysList.add(_eventsByCategoryKey);
+      if (result?.topSellingEvents != null) keysList.add(_top3EventsKey);
+      if (result?.mostViewedEvents != null) keysList.add(_mostViewedEventsKey);
+      if (result?.mostSavedEvents != null) keysList.add(_mostSavedEventsKey);
+
+      for (var key in keysList) {
+        var imageBytes = await _captureWidgetAsImage(key);
+        var pwImage = pw.MemoryImage(imageBytes);
+        chartImagesList.add(pwImage);
+      }
+
+      pdf.addPage(
+        pw.MultiPage(
+          build: (context) {
+            List<pw.Widget> widgets = [];
+
+            // Add the header
+            widgets.add(
+              pw.Center(
+                  child: pw.Text(
+                "Dogadjaji izvjestaj",
+                style: pw.TextStyle(
+                    fontSize: 16,
+                    fontWeight: pw.FontWeight.bold,
+                    color: PdfColor(0.5, 0.5, 0.5)),
+              )),
+            );
+            widgets.add(
+                pw.SizedBox(height: 20)); // Space between header and content
+
+            // Add the charts
+            for (var chart in chartImagesList) {
+              widgets.add(pw.Center(child: pw.Image(chart)));
+              widgets
+                  .add(pw.SizedBox(height: 20)); // Add spacing between charts
+            }
+
+            return widgets;
+          },
+        ),
+      );
+
+      // Save or share the PDF
+      final outputDir = await getDownloadsDirectory();
+      if (outputDir == null) {
+        throw Exception("Error: Downloads directory could not be found");
+      }
+
+      var dateNow = DateTime.now();
+      final outputPath =
+          "${outputDir.path}\\DogadjajiReport-${dateNow.day}${dateNow.month}${dateNow.year}.pdf";
+
+      final pdfFile = File(outputPath);
+      await pdfFile.writeAsBytes(await pdf.save());
+      print("PDF saved to: $outputPath");
+    }
+    // Optionally open the file or inform the user
+    catch (e) {
+      print("Error while exporting PDF: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MasterScreenWidget(
@@ -65,35 +160,59 @@ class _DogadjajIzvjestajScreenState extends State<DogadjajIzvjestajScreen> {
                     padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
                     child: SingleChildScrollView(
                         child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text("Događaji izvještaj",
-                            textAlign: TextAlign.start,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 91, 91, 91),
-                            )),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        result?.eventsByStatus != null
-                            ? _buildEventsByStatus()
-                            : Container(),
-                        result?.eventsByCategory != null
-                            ? _buildEventsByCategory()
-                            : Container(),
-                        result?.topSellingEvents != null
-                            ? _buildTop3Events()
-                            : Container(),
-                        result?.mostViewedEvents != null
-                            ? _buildMostViewedEvents()
-                            : Container(),
-                        result?.mostSavedEvents != null
-                            ? _buildMostSavedEvents()
-                            : Container(),
-                      ],
-                    )))));
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text("Događaji izvještaj",
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(255, 91, 91, 91),
+                                    )),
+                                IconButton(
+                                    onPressed: exportPdf,
+                                    tooltip: "Download pdf",
+                                    icon: Icon(
+                                      Icons.download,
+                                      color: Colors.grey,
+                                    ))
+                              ]),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                result?.eventsByStatus != null
+                                    ? RepaintBoundary(
+                                        key: _eventsByStatusKey,
+                                        child: _buildEventsByStatus())
+                                    : Container(),
+                                result?.eventsByCategory != null
+                                    ? RepaintBoundary(
+                                        key: _eventsByCategoryKey,
+                                        child: _buildEventsByCategory())
+                                    : Container(),
+                                result?.topSellingEvents != null
+                                    ? RepaintBoundary(
+                                        key: _top3EventsKey,
+                                        child: _buildTop3Events())
+                                    : Container(),
+                                result?.mostViewedEvents != null
+                                    ? RepaintBoundary(
+                                        key: _mostViewedEventsKey,
+                                        child: _buildMostViewedEvents())
+                                    : Container(),
+                                result?.mostSavedEvents != null
+                                    ? RepaintBoundary(
+                                        key: _mostSavedEventsKey,
+                                        child: _buildMostSavedEvents())
+                                    : Container(),
+                              ])
+                        ])))));
   }
 
 //pie
